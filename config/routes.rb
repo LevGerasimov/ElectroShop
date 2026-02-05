19cfc5eda8ab01600680bb252fe69eeaa66cede8
naetdragon1-crypto <naet.dragon.1@gmail.com>
2026-02-05 19:00:52 +0100

    pContacts

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get "contacts", to: "contacts#index", as: :contacts
+  # get "contacts", to: "users#show"
   # get "main/index"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
